47f888e70de53858ccf2bfab2db7652ae3cc5c66
sguazt <marco.guazzone@gmail.com>
2013-10-31 11:42:43 +0100

    [php] (change:minor) Initialized variable.

--- webapp/php/trunk/controllers/Tags_Controller.php
+++ webapp/php/trunk/controllers/Tags_Controller.php
@@ -55,6 +55,7 @@ class Tags_Controller {
             $step = ($max_size - $min_size)/($spread);
 
             // loop through our tag array
+			$homePageTagCloud = '';
             foreach ($tags as $key => $value) {
                 $size = $min_size + (($value - $min_count) * $step);
                 // uncomment if you want sizes in whole %:
